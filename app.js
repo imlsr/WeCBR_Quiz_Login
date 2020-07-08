41f7b42892a2e6727af3af9bbfbda6e128d4999a
imlsr <likhith.sr@studentpartner.com>
2020-07-08 23:14:27 +0530

    Added Passport-local-mongoose

--- app.js
+++ app.js
@@ -11,6 +11,41 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(session({
+  secret: "wecbr-quiz-login",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/UserDB", {useNewUrlParser : true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+  firstName : String,
+  lastName : String,
+  username : String,
+  password : String
+
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
+
+//ROUTES-GET-START
 app.get("/quiz/login" , function(req , res){
   res.sendFile(__dirname + '/login.html');
 })
@@ -19,41 +54,53 @@ app.get("/quiz/register" , function(req , res){
   res.sendFile(__dirname + '/register.html');
 })
 
+app.get("/quiz/dashboard" , function(req, res){
+  if(req.isAuthenticated()){
+    //Open Dashboard
+  }else {
+    res.redirect("/quiz/login");
+  }
 
-mongoose.connect("mongodb://localhost:27017/UserDB", {useNewUrlParser : true});
+});
+//ROUTES-GET-END
 
-const userSchema = {
-  firstName : String,
-  lastName : String,
-  email : String,
-  password : String
 
-};
 
-const User = new mongoose.model("User", userSchema);
 
+//ROUTES-POST-START
 app.post("/quiz/register" , function(req , res){
-  const newUser = new User({
-    firstName : req.body.firstName,
-    lastName : req.body.lastName,
-    email : req.body.email,
-    password : req.body.pass
+
+  User.register({username:req.body.email , firstName : req.body.firstName , lastName : req.body.lastName }, req.body.pass , function(err, user){
+    if(err) {
+      console.log(err);
+      res.redirect("/quiz/register");
+    }else{
+      passport.authenticate("local")(req , res , function(){
+        res.redirect("/quiz/dashboard")
+      });
+    }
   });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.sendFile(__dirname + '/login.html');
-    }
-  })
-})
+});
 
 app.post("/quiz/login" , function(req , res){
 
+  const user = new User({
+    username: req.body.email,
+    password: req.body.pass
+  });
+
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req , res , function(){
+        res.redirect("/quiz/dashboard")
+    }
+  })
 
 })
-
+//ROUTES-POST-END
 
 
 
